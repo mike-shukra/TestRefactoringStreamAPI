ad63e4e42d0bbe8acbfbf46cb50e73d2eded93cf
Mikhail Shkuro <white.electric.dog@gmail.com>
2020-02-18 13:27:57 +0300

    Create

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,39 @@
+import data.Member;
+import data.MembersGroup;
+import service.Finder;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MainTest {
+
+    private static Finder finder = new Finder();
+    private static List<MembersGroup> groups = new ArrayList<>();
+    private static Set<String> mySet = new HashSet<>();
+
+    public static void main(String[] args) {
+        groups.add(new MembersGroup("group01", createMembers(20)));
+        groups.add(new MembersGroup("group02", createMembers(30)));
+        groups.add(new MembersGroup("group03", createMembers(40)));
+        mySet = finder.findOldMembers(groups, 30);
+
+        System.out.println("test: ");
+        for (String group: mySet) {
+            System.out.println(group);
+        }
+    }
+
+    private static List<Member> createMembers(int age){
+
+        List<Member> members = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            String name = ("name " + i + " age: " + age);
+            members.add(new Member(name, age));
+        }
+        return members;
+    }
+
+}
